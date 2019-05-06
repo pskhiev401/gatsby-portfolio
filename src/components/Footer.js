2787e284083822ca6139e781c520cdd3eecccbf1
Patric Khiev <webdev.patric@gmail.com>
2019-05-06 08:48:19 -0700

    fixed twitter handle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><a href="https://www.linkedin.com/in/pskhiev" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
                         <li><a href="https://github.com/pskhiev401" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="https://twitter.com/patrickhiev" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://twitter.com/pateekakes" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                         <li><a href="mailto:webdev.patric@gmail.com" className="icon fa-envelope"><span className="label">Email</span></a></li>
                     </ul>
                     {/* <ul className="copyright">
